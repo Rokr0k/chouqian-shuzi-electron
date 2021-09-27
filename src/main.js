0bace24636477a3c269ec526bbd1136f6d4bd6cb
Rokr0k <yangeh2225@gmail.com>
2021-09-27 13:10:13 +0000

    add anniversaries

--- src/main.js
+++ src/main.js
@@ -3,14 +3,34 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow;
 
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        title: "抽签数字",
-        icon: `${__dirname}/../public/icon/png/icon.png`,
-        autoHideMenuBar: true,
-    });
-    mainWindow.maximize();
+    const date = new Date();
 
-    mainWindow.loadURL(`file://${__dirname}/../public/html/index.html`);
+    if (date.getMonth() === 3 && date.getDate() === 1) {
+        mainWindow = new BrowserWindow({
+            title: "ᡨᠣᠨ ᡨᠠᡨᠠᠮᠪᡳ",
+            icon: `${__dirname}/../public/icon/png/icon.png`,
+            autoHideMenuBar: true,
+        });
+        mainWindow.loadURL(`file://${__dirname}/../public/html/april-fools.html`);
+    }
+    else if (date.getMonth() === 9 && date.getDate() === 9) {
+        mainWindow = new BrowserWindow({
+            title: "튜ᇢ쳠쵹ᄍᆞᆼ",
+            icon: `${__dirname}/../public/icon/png/icon.png`,
+            autoHideMenuBar: true,
+        });
+        mainWindow.loadURL(`file://${__dirname}/../public/html/hangul.html`);
+    }
+    else {
+        mainWindow = new BrowserWindow({
+            title: "抽签数字",
+            icon: `${__dirname}/../public/icon/png/icon.png`,
+            autoHideMenuBar: true,
+        });
+        mainWindow.loadURL(`file://${__dirname}/../public/html/index.html`);
+    }
+
+    mainWindow.maximize();
 
     mainWindow.on('closed', () => {
         mainWindow = null;
